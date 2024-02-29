aca022facb35801489c2da2e4c794ca6b24f4de0
HarryFelty <harryfelty@gmail.com>
2024-02-29 11:15:18 -0500

    updated resume

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -12,8 +12,8 @@ export default function AboutPage() {
                     <p> I am a full-stack web developer. About a year ago I graduated from Penn State with a B.S. is cybersecurity. Since then, I have worked in
                         IT and obtained my CompTIA Security+ Certificate. I was recently introduced to programming and found that I
                         greatly enjoy it. After spending some time self-learning and exploring the field, I decided to join a bootcamp
-                        to help hone my skills. I attended the edX bootcamp through the University of Pennsylvania. The bootcamp taught me many technologies that are used
-                        in full-stack development. As I continue to grow as a developer and perfect the skills I have already learned, I am looking for a fulfilling career
+                        to help develop my skills. I attended the edX bootcamp through the University of Pennsylvania. The bootcamp taught me many technologies that are used
+                        in full-stack development. As I continue to grow as a developer, I am looking for a fulfilling career
                         as a full-stack web developer. </p>
                 </div>
             </div>
